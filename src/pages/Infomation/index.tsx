41b783b6cb65a1629b96713500a7553b10c70b62
LI WANG <zoeywang1990@gmail.com>
2020-02-15 20:52:05 +1100

    infomation page

--- src/pages/Infomation/index.tsx
+++ src/pages/Infomation/index.tsx
@@ -1,16 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './style.less';
 import { AnimationWrapper } from '../../components/AnimationWrapper';
 
 export default function() {
-  const [inProp, setInProp] = useState(false);
   return (
-    <AnimationWrapper >
-      <div>
-        INFOMATION---INFOMATION--INFOMATIONINFOMATION INFOMATION---INFOMATION--INFOMATIONINFOMATION
-        INFOMATION---INFOMATION--INFOMATIONINFOMATION INFOMATION---INFOMATION--INFOMATIONINFOMATION
-        INFOMATION---INFOMATION--INFOMATIONINFOMATION INFOMATION---INFOMATION--INFOMATIONINFOMATION
-        INFOMATION---INFOMATION--INFOMATIONINFOMATION INFOMATION---INFOMATION--INFOMATIONINFOMATION
+    <AnimationWrapper>
+      <div className={styles.wrapper}>
+        <div className={styles.textContainer}>
+          <div className={styles.mb15}>
+            <p>
+              Yingda Xu(徐英达) is a Chinese film photographer and graphic designer currently based
+              in Guangzhou, China.
+            </p>
+            <p>
+              In 2015, he graduated from Swinburne University in Melbourne, Australia with a
+              bachelor of Visual Communication Design specialising in Photography.
+            </p>
+          </div>
+          <p className={styles.mb15}>For business inquiries please contact me via email.</p>
+          <div className={styles.mb55}>
+            <p>
+              <b>Email:</b> yingdaxu@outlook.com
+            </p>
+            <p>
+              <b>Wechat:</b> Shawnxu92
+            </p>
+            <p></p>
+          </div>
+          <div>
+            <p>
+              <b>Press:</b>
+            </p>
+            <p>Shortlisted - Palm Photo Prize</p>
+            <p>Aint-bad, Ayemag, Pample-Mousse, Altair Zine, </p>
+          </div>
+        </div>
       </div>
     </AnimationWrapper>
   );
